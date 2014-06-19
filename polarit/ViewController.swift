c69b2720786d83ef7f10630d794b7d4c7ea79be3
Atanas Balevsky <atanas.balevsky@gmail.com>
2014-06-19 16:12:09 +0300

    initial + translated example from nshipster

--- polarit/ViewController.swift
+++ polarit/ViewController.swift
@@ -19,7 +19,44 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        /*
+GPUImageVideoCamera *videoCamera = [[GPUImageVideoCamera alloc]
+initWithSessionPreset:AVCaptureSessionPreset640x480
+cameraPosition:AVCaptureDevicePositionBack];
 
+        videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+
+GPUImageFilter *filter = [[GPUImageLevelsFilter alloc] initWithFragmentShaderFromFile:@"CustomShader"];
+[filter setRedMin:0.299 gamma:1.0 max:1.0 minOut:0.0 maxOut:1.0];
+[filter setGreenMin:0.587 gamma:1.0 max:1.0 minOut:0.0 maxOut:1.0];
+[filter setBlueMin:0.114 gamma:1.0 max:1.0 minOut:0.0 maxOut:1.0];
+[videoCamera addTarget:filter];
+
+GPUImageView *filteredVideoView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+[filter addTarget:filteredVideoView];
+[self.view addSubview:filteredVideoView];
+
+[videoCamera startCameraCapture];
+*/
+        let videoCamera = GPUImageVideoCamera(sessionPreset: AVCaptureSessionPreset1280x720, cameraPosition: .Back)
+        videoCamera.outputImageOrientation = .Portrait
+        
+        let filter = GPUImageLevelsFilter(fragmentShaderFromFile: "CustomShader")
+        filter.setRedMin(0.299, gamma:1.0, max:1.0, minOut:0.0, maxOut:1.0)
+        filter.setGreenMin(0.587, gamma:1.0, max:1.0, minOut:0.0, maxOut:1.0)
+        filter.setBlueMin(0.114, gamma:1.0, max:1.0, minOut:0.0, maxOut:1.0)
+        videoCamera.addTarget(filter)
+        
+        let filteredVideoView = GPUImageView(frame: self.view.bounds)
+        filter.addTarget(filteredVideoView)
+        view.addSubview(filteredVideoView)
+        
+        videoCamera.startCameraCapture()
+    }
 
 }
 
---
+++ polarit/polarit-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  polarit-Bridging-Header.h.h
+//  polarit
+//
+//  Created by Atanas Balevsky on 6/19/14.
+//  Copyright (c) 2014 Atanas Balevsky. All rights reserved.
+//
+
+
+#import <GPUImage/GPUImage.h>
